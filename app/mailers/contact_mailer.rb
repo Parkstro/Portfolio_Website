c590de13e39be1933fb18566a03177bc52cdf22e
Parker Strom <stromparker@comcast.net>
2017-02-15 19:08:57 +0000

    updated where the contact form sends to

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'stromparker@gmail.com'
+  default to: 'parker@parkerstrom.com'
   def contact_email(name, email, body)
     @name = name
     @email = email
