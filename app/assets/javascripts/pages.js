0bc2e03a3b9bdd7afa83bf5a88779ea607ee44c4
Parker Strom <stromparker@comcast.net>
2017-04-14 18:12:05 +0000

    added parallax plugin for awesome mouseover effects

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,33 +1,38 @@
-/* global $ jQuery */
+/* global $ jQuery finish */
 
 $(document).on('turbolinks:load', function(){
     var small={ 
                 display: "block",
-                position: "static",
-                 borderTop: "transparent",
-                 borderLeft: "transparent" ,
-                 borderRight: "transparent",
-                 borderBottom: "transparent",
-                 borderStyle: "solid",
-                 borderBottomWidth: "100%",
-                 borderTopWidth: "100%",
-                 borderLeftWidth: "100%",
-                 borderRightWidth: "100%",
+                position: "relative",
+                left: "auto",
+                marginLeft: "",
+                borderTop: "transparent",
+                borderLeft: "transparent" ,
+                borderRight: "transparent",
+                borderBottom: "transparent",
+                borderStyle: "solid",
+                borderBottomWidth: "100%",
+                borderTopWidth: "100%",
+                borderLeftWidth: "100%",
+                borderRightWidth: "100%",
                 width: "100%", 
                 borderRadius: "5px", 
                 zIndex: "9"
     };
                 
-    var large={  display: "block",
-                 position: "absolute",
-                 width: "1000px",
-                 borderBottomColor: "rgba(0,0,0,0.8)",
-                 borderLeftColor: "rgba(0,0,0,0.8)" ,
-                 borderRightColor: "rgba(0,0,0,0.8)",
-                 borderTopColor: "rgba(0,0,0,0.8)",
-                 borderstyle: "solid",
-                 zIndex: "9",
-                 borderRadius: "5px"
+    var large={  
+                display: "block",
+                position: "fixed",
+                left: "25%",
+                marginLeft: "-6.25em",
+                width: "150%",
+                borderBottomColor: "rgba(0,0,0,0.8)",
+                borderLeftColor: "rgba(0,0,0,0.8)" ,
+                borderRightColor: "rgba(0,0,0,0.8)",
+                borderTopColor: "rgba(0,0,0,0.8)",
+                borderstyle: "solid",
+                zIndex: "9",
+                borderRadius: "5px"
      };
     var count=1;
     jQuery("img").css(small).on('click', function(){
