82ba42fa45f8011059f100a250e2309d8f4f7a54
Parker Strom <stromparker@comcast.net>
2017-04-14 18:43:12 +0000

    fixed positioning issues when enlarging and returning pictures

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -4,17 +4,9 @@ $(document).on('turbolinks:load', function(){
     var small={ 
                 display: "block",
                 position: "relative",
-                left: "auto",
+                left: "",
                 marginLeft: "",
-                borderTop: "transparent",
-                borderLeft: "transparent" ,
-                borderRight: "transparent",
-                borderBottom: "transparent",
-                borderStyle: "solid",
-                borderBottomWidth: "100%",
-                borderTopWidth: "100%",
-                borderLeftWidth: "100%",
-                borderRightWidth: "100%",
+                backgroundColor: "transparent",
                 width: "100%", 
                 borderRadius: "5px", 
                 zIndex: "9"
@@ -25,17 +17,14 @@ $(document).on('turbolinks:load', function(){
                 position: "fixed",
                 left: "25%",
                 marginLeft: "-6.25em",
-                width: "150%",
-                borderBottomColor: "rgba(0,0,0,0.8)",
-                borderLeftColor: "rgba(0,0,0,0.8)" ,
-                borderRightColor: "rgba(0,0,0,0.8)",
-                borderTopColor: "rgba(0,0,0,0.8)",
+                width: "125%",
+                backgroundColor: "rgba(0,0,0,0.8)",
                 borderstyle: "solid",
                 zIndex: "9",
                 borderRadius: "5px"
      };
     var count=1;
-    jQuery("img").css(small).on('click', function(){
+    jQuery(".col-md-8 > img").css(small).on('click', function(){
         jQuery(this).animate((count==1)?large:small);
         count = 1-count;
     });
